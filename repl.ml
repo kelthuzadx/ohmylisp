e22e470ab13fe73bf8f253898cdd6ae39d3789f3
kelthuzadx <1948638989@qq.com>
2020-07-01 21:21:50 +0800

    parse_num

--- repl.ml
+++ repl.ml
@@ -1,12 +1,35 @@
-open String;;
-
 type value =
   | Num of int
 ;;
 
-type given = {input:string;  cur:int ;max_len:int}
+type given = {
+  input:string;
+  mutable  cur:int;
+  max_len:int
+}
 
 exception SyntaxError of string;;
+
+let rec parse_num g n= 
+  if g.cur<g.max_len
+  then
+    match g.input.[g.cur] with 
+    | '0'..'9' -> 
+      let c = Char.escaped g.input.[g.cur]
+      in 
+      g.cur<-g.cur+1;
+      parse_num g (n ^ c)
+    | _ ->  
+      n
+  else
+    n
+;;
+
+let parse_sexpr g = 
+  match g.input.[g.cur] with 
+  | '0'..'9' -> print_string (parse_num g "")
+  | _ -> print_int 0
+;;
 
 let repl = 
   while true do
@@ -15,7 +38,7 @@ let repl =
     in
     let g = {input=input;cur=0;max_len=String.length input}
     in 
-    print_string g.input;
+    parse_sexpr g;
     print_newline ();
     flush stdout;
   done
