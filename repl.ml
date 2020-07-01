2ffe48d2f0563c1bb353eb62a770e1f154571949
kelthuzadx <1948638989@qq.com>
2020-07-01 22:02:25 +0800

    parse_boolg

--- repl.ml
+++ repl.ml
@@ -1,6 +1,9 @@
-type value =
-  | Num of int
-;;
+(*
+Language Specification
+
+number: 23,12,32
+boolean: #t,#f
+*)
 
 type given = {
   input:string;
@@ -8,28 +11,54 @@ type given = {
   max_len:int
 }
 
+type value =
+  | Num of int
+  | Bool of bool
+;;
+
 exception SyntaxError of string;;
 
-let rec parse_num g n= 
-  if g.cur<g.max_len
-  then
-    match g.input.[g.cur] with 
-    | '0'..'9' -> 
-      let c = Char.escaped g.input.[g.cur]
-      in 
-      g.cur<-g.cur+1;
-      parse_num g (n ^ c)
-    | _ ->  
+let parse_num g =
+  let rec parse_num_helper g n= 
+    if g.cur<g.max_len
+    then
+      match g.input.[g.cur] with 
+      | '-' | '0'..'9' -> 
+        let c = Char.escaped g.input.[g.cur]
+        in 
+        g.cur<-g.cur+1;
+        parse_num_helper g (n ^ c)
+      | _ ->  
+        n
+    else
       n
-  else
-    n
+  in
+  let str = parse_num_helper g ""
+  in
+  Num (int_of_string str) 
+;;
+
+let parse_bool g= 
+  g.cur <- g.cur+1;
+  match g.input.[g.cur] with
+  | 't' -> Bool true
+  | 'f' -> Bool false
+  | _ -> raise (SyntaxError "expect #t or #f of bool type\n")
+;;
+
+let print_value a = 
+  match a with
+  | Num v -> print_string (string_of_int v)
+  | Bool v -> if v=true then print_string "#t" else print_string "#f"
 ;;
 
 let parse_sexpr g = 
   match g.input.[g.cur] with 
-  | '0'..'9' -> print_string (parse_num g "")
-  | _ -> print_int 0
+  | '-' | '0'..'9' -> print_value (parse_num g)
+  | '#' -> print_value (parse_bool g)
+  | _ -> print_string "Error"
 ;;
+
 
 let repl = 
   while true do
