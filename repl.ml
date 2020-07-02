3c0c914406fcb23202f8c468483735a319447179
kelthuzadx <1948638989@qq.com>
2020-07-02 12:26:09 +0800

    parse symbol

--- repl.ml
+++ repl.ml
@@ -1,8 +1,9 @@
 (*
 Language Specification
 
-number: 23,12,32
+number: 23,12,~32
 boolean: #t,#f
+symbol: hello,yang-yi,+,empty?
 *)
 
 type given = {
@@ -14,26 +15,30 @@ type given = {
 type value =
   | Num of int
   | Bool of bool
+  | Symbol of string
 ;;
 
 exception SyntaxError of string;;
 
 let parse_num g =
-  let rec parse_num_helper g n= 
+  let rec helper g n= 
     if g.cur<g.max_len
     then
       match g.input.[g.cur] with 
-      | '-' | '0'..'9' -> 
+      | '~'->
+        g.cur <- g.cur+1;
+        helper g ( n^"-")
+      | '0'..'9' -> 
         let c = Char.escaped g.input.[g.cur]
         in 
         g.cur<-g.cur+1;
-        parse_num_helper g (n ^ c)
+        helper g (n ^ c)
       | _ ->  
         n
     else
       n
   in
-  let str = parse_num_helper g ""
+  let str = helper g ""
   in
   Num (int_of_string str) 
 ;;
@@ -46,16 +51,40 @@ let parse_bool g=
   | _ -> raise (SyntaxError "expect #t or #f of bool type\n")
 ;;
 
+let parse_symbol g =
+  match g.input.[g.cur] with 
+  |'*'|'/'|'>'|'<'|'='|'?'|'!'|'-'|'+'|'A'..'Z'|'a'..'z'-> 
+    let rec helper g n= 
+      if g.cur<g.max_len
+      then
+        match g.input.[g.cur] with 
+        |'*'|'/'|'>'|'<'|'='|'?'|'!'|'-'|'+'|'A'..'Z'|'a'..'z'-> 
+          let c = Char.escaped g.input.[g.cur]
+          in 
+          g.cur<-g.cur+1;
+          helper g (n ^ c)
+        | _ ->  
+          Symbol n
+      else
+        Symbol n
+    in
+    helper g ""
+  |_-> raise (SyntaxError "expect symbol here")
+;;
+
 let print_value a = 
   match a with
   | Num v -> print_string (string_of_int v)
   | Bool v -> if v=true then print_string "#t" else print_string "#f"
+  | Symbol v -> print_string v
 ;;
 
-let parse_sexpr g = 
+let parse_sexpr g =
   match g.input.[g.cur] with 
-  | '-' | '0'..'9' -> print_value (parse_num g)
+  | '~' | '0'..'9' -> print_value (parse_num g)
   | '#' -> print_value (parse_bool g)
+  |'*'|'/'|'>'|'<'|'='|'?'|'!'|'-'|'+'|'A'..'Z'|'a'..'z'-> 
+    print_value (parse_symbol g) 
   | _ -> print_string "Error"
 ;;
 
